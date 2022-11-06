192aea407e605f725588ebf482f39e8a050662e6
rahulTPatil <31767757+rahulTPatil@users.noreply.github.com>
2022-11-06 13:39:52 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nativescript-svelte-demo
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nativescript-stackblitz-templates-pvzahr)
---
+++ app/pages/Details.svelte
@@ -0,0 +1,37 @@
+
+<page> 
+<actionBar title="{flick.title}" />
+
+  <!-- Add this 👇 -->
+  <scrollView>
+    <stackLayout>
+      <image margin="0" stretch="aspectFill" src="https://hatrabbits.com/wp-content/uploads/2017/01/random-word-1.jpg" />
+      <stackLayout padding="10 20">
+        {#each flick.details as details}
+        <stackLayout>
+          <label
+            marginTop="15"
+            fontSize="16"
+            fontWeight="700"
+            class="text-primary"
+            textWrap="true"
+            text="{details.title}"
+          />
+          <label
+            fontSize="14"
+            class="text-secondary"
+            textWrap="true"
+            text="{details.body}"
+          />
+        </stackLayout>
+        {/each}
+      </stackLayout>
+    </stackLayout>
+  </scrollView>
+</page>
+
+<script>
+import { FlickService } from '../services/flickService'
+export let flickId
+let flick = FlickService.getInstance().getFlickById(flickId)
+</script>
